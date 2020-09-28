1d6b04e67a2f28d8165fac0a0c3883184b995593
Alfredo Martinez <martin3zra@gmail.com>
2020-09-28 19:54:48 -0400

    add sluglify helpers

---
+++ Sources/SwiftX/Extensions/String.swift
@@ -0,0 +1,28 @@
+//
+//  String.swift
+//  
+//
+//  Created by Alfredo Martinez on 9/28/20.
+//
+
+#if !os(macOS)
+import UIKit
+
+@available(iOS 11.0, tvOS 11.0, *)
+extension String {
+    private static let slugSafeCharacters = CharacterSet(charactersIn: "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-")
+
+    public func convertedToSlug() -> String? {
+        if let latin = self.applyingTransform(StringTransform("Any-Latin; Latin-ASCII; Lower;"), reverse: false) {
+            let urlComponents = latin.components(separatedBy: String.slugSafeCharacters.inverted)
+            let result = urlComponents.filter { $0 != "" }.joined(separator: "-")
+
+            if result.count > 0 {
+                return result
+            }
+        }
+
+        return nil
+    }
+}
+#endif
